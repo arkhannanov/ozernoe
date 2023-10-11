773d02deb83dd646429760ccf990c320825fe182
Artur K <arkhannanov@gmail.com>
2023-10-11 17:44:19 +0300

    initial commit

--- ozernoe/app/components/MainWrapper/MainWrapper.tsx
+++ ozernoe/app/components/MainWrapper/MainWrapper.tsx
@@ -1,8 +1,9 @@
 import { Button, StyledEngineProvider } from '@mui/material'
 import Image from 'next/image'
 import styles from "./styles.module.css"
+import { ReactNode } from 'react'
 
-const MainWrapper = ({children}) => {
+const MainWrapper = ({children}: { children: ReactNode}) => {
   return (
         <div className={styles['mainWrapper']}>
             {children}
