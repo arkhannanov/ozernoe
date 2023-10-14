7f5ba81d73219f62d883917ea6514d0b27aa1d53
Artur K <arkhannanov@gmail.com>
2023-10-14 18:10:14 +0300

    phkhkhkhk

--- ozernoe/next.config.js
+++ ozernoe/next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+	trailingSlash: false,
+	webpack: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		}
+		
+		return config
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')]
+	},
+	/**
+	 * We specify which domains are allowed to be optimized.
+	 * This is needed to ensure that external urls can't be abused.
+	 * @see https://nextjs.org/docs/basic-features/image-optimization#domains
+	 */
+}
